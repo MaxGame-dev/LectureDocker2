db1d21cda4196fb12c2128e95fdb19c555c903e3
MSI\tttmd <azm.game.dev@gmail.com>
2025-08-07 16:26:34 +0900

    ガチャ作成用のデータ準備

---
+++ sql/initialize.sql
@@ -0,0 +1,45 @@
+CREATE USER IF NOT EXISTS 'data_user'@'localhost' IDENTIFIED BY 'data';
+GRANT ALL PRIVILEGES ON * . * TO 'data_user'@'localhost';
+
+CREATE USER IF NOT EXISTS 'data_user'@'%' IDENTIFIED BY 'data';
+GRANT ALL PRIVILEGES ON * . * TO 'data_user'@'%';
+
+DROP DATABASE IF EXISTS lecture_db;
+CREATE DATABASE IF NOT EXISTS lecture_db;
+
+use lecture_db;
+
+DROP TABLE IF EXISTS items;
+CREATE TABLE IF NOT EXISTS items (
+    item_id INT PRIMARY KEY,
+    item_name VARCHAR(255)
+);
+
+insert into items (item_id, item_name) values (1, '神レアアイテム');
+insert into items (item_id, item_name) values (2, '超レアアイテム');
+insert into items (item_id, item_name) values (3, 'レアアイテム');
+insert into items (item_id, item_name) values (4, '普通のアイテム');
+insert into items (item_id, item_name) values (5, 'ガラクタ');
+
+
+DROP TABLE IF EXISTS gacha_items;
+CREATE TABLE IF NOT EXISTS gacha_items (
+    gacha_id INT,
+    item_id INT,
+    weight INT,
+    PRIMARY KEY (gacha_id, item_id)
+);
+
+insert into gacha_items (gacha_id, item_id, weight) values (1, 1, 10);
+insert into gacha_items (gacha_id, item_id, weight) values (1, 2, 90);
+insert into gacha_items (gacha_id, item_id, weight) values (1, 3, 200);
+insert into gacha_items (gacha_id, item_id, weight) values (1, 4, 300);
+insert into gacha_items (gacha_id, item_id, weight) values (1, 5, 400);
+
+DROP TABLE IF EXISTS gacha_histories;
+CREATE TABLE IF NOT EXISTS gacha_histories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    history_id INT,
+    gacha_id INT,
+    item_id INT
+);
